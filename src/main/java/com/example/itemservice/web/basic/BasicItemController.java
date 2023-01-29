6a58df3c7827349d455e45a11a3760205424ca73
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-29 21:50:31 +0900

    feat: PRG 적용

--- src/main/java/com/example/itemservice/web/basic/BasicItemController.java
+++ src/main/java/com/example/itemservice/web/basic/BasicItemController.java
@@ -56,10 +56,16 @@ public class BasicItemController {
         return "basic/item";
     }
 
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItemV3(Item item){
         itemRepository.save(item);
         return "basic/item";
+    }
+
+    @PostMapping("/add")
+    public String addItemV4(Item item){
+        itemRepository.save(item);
+        return "redirect:/basic/items/"+item.getId();
     }
 
     @GetMapping("/{itemId}/edit")
